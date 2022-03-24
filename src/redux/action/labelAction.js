cd15803583369ed05ea2df6d72ff8744b5b1a5c4
MasonNguyen <mason_nguyen@mason.nguyen>
2022-03-24 14:48:36 +0700

    update alert of label

--- src/redux/action/labelAction.js
+++ src/redux/action/labelAction.js
@@ -54,6 +54,10 @@ export const getLabelName = (label_id) => {
 export const createLabels = (label) => {
   return async (dispatch) => {
     try {
+      Toast.fire({
+        icon: "info",
+        title: "Your label is creating",
+      });
       const result = await googleKeepApi.createLabel(label);
       Toast.fire({
         icon: "success",
@@ -73,10 +77,14 @@ export const editLabel = (label) => {
   return async (dispatch) => {
     try {
       Toast.fire({
+        icon: "info",
+        title: "Your label is editing",
+      });
+      const result = await googleKeepApi.editLabel(label);
+      Toast.fire({
         icon: "success",
         title: "Your label have been updated",
       });
-      const result = await googleKeepApi.editLabel(label);
 
       dispatch({
         type: UPDATE_LABEL,
@@ -93,10 +101,14 @@ export const deleteLabel = (label) => {
   return async (dispatch) => {
     try {
       Toast.fire({
+        icon: "info",
+        title: "Your label is deleting",
+      });
+      const result = await googleKeepApi.deleteLabel(label);
+      Toast.fire({
         icon: "success",
         title: "Your label have been deleted",
       });
-      const result = await googleKeepApi.deleteLabel(label);
       dispatch({
         type: DELETE_LABEL,
         newArrLabel: result.newArrLabels,
