adc6a43f30a9ab8c4209e3a28d7e64340924994f
MasonNguyen <mason_nguyen@mason.nguyen>
2022-03-24 16:13:52 +0700

    replace self

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
 import { precacheAndRoute } from "workbox-precaching";
-var self = this;
-precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener("push", (event) => {
+precacheAndRoute(this.__WB_MANIFEST);
+
+this.addEventListener("push", (event) => {
   const payload = event.data.json();
   event.waitUntil(
-    self.registration.showNotification(payload.title.replace(/<[^>]+>/g, ""), {
+    this.registration.showNotification(payload.title.replace(/<[^>]+>/g, ""), {
       body: payload.content.replace(/<[^>]+>/g, ""),
       icon: "./favicon.png",
       vibrate: [200, 100, 200],
